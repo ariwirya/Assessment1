fb62635d6fac91f4cec976074461a6b06633f2ee
ariwirya <ariwirya00@gmail.com>
2023-03-27 09:38:52 +0700

    v1.2.1 Membuat isi proses.php dengan operasi aritmatika dengan switch case

---
+++ proses.php
@@ -0,0 +1,25 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	$angka1 = $_POST['angka1'];
+	$angka2 = $_POST['angka2'];
+	$operator = $_POST['operator'];
+
+	switch ($operator) {
+		case '+':
+			$hasil = $angka1 + $angka2;
+			break;
+		case '-':
+			$hasil = $angka1 - $angka2;
+			break;
+		case '*':
+			$hasil = $angka1 * $angka2;
+			break;
+        case '/':
+            $hasil = $angka1 * $angka2;
+            break;
+		default:
+			$hasil = 'Operator tidak valid!';
+			break;
+	}
+}
+?>
